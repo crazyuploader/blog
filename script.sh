d37fe773d70fc41c199728ebe3a8fdc4982120d3
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-05-19 15:48:19 +0530

    Add School Project Website

--- script.sh
+++ script.sh
@@ -8,11 +8,25 @@ rm -rf hugo.deb
 # Building site
 git clone https://"${GITHUB_REF}" -b master ~/public
 hugo -d ~/public
-cd ~/public
+cd ~/public || exit 1
+
+# Custom Function to get school website project
+function GET_SCHOOL() {
+    cd /tmp || exit 1
+    curl -sLo school.zip https://github.com/crazyuploader/school/archive/master.zip
+    unzip -d temp/ school.zip
+    rm -rf school.zip
+    cd temp || exit 1
+    mv school-master school
+    cd ..
+    mv school/ ~/public
+    cd ~/public || exit 1
+}
 
 # Pushing built to GitHub
 git config user.email "49350241+crazyuploader@users.noreply.github.com"
 git config user.name "crazyuploader"
+GET_SCHOOL
 echo ""
 git push https://crazyuploader:"${GITHUB_TOKEN}"@"${GITHUB_REF}" HEAD:master
 echo ""

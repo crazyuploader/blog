2eb69146342daad68611e34c7de59c86a6509b2a
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-05-19 14:22:56 +0530

    Update Script

--- script.sh
+++ script.sh
@@ -1,1 +1,20 @@
-echo "Something here"
+#!/usr/bin/env bash
+
+# Installing Hugo
+curl -sLo hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.71.0/hugo_0.71.0_Linux-64bit.deb
+sudo dpkg -i hugo.deb
+rm -rf hugo.deb
+
+# Building site
+git clone https://"${GITHUB_REF}" -b master ~/public
+hugo -d ~/public
+cd ~/public
+
+# Pushing built to GitHub
+echo ""
+git config user.email "49350241+crazyuploader@users.noreply.github.com"
+git config user.name "crazyuploader"
+echo ""
+git push https://crazyuploader:"${GITHUB_TOKEN}"@"${GITHUB_REF}" HEAD:master
+echo ""
+echo "Done"

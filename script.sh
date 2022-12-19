9bd5ace8bf599cfe643f40975666a8c1c9cdedcd
Jugal Kishore <me@devjugal.com>
2022-12-19 16:49:00 +0530

    Update script.sh
    Remove including School Website Project

--- script.sh
+++ script.sh
@@ -35,23 +35,9 @@ echo -e "${YELLOW}Building site with Hugo${NC}"
 hugo -d ~/public
 cd ~/public || exit 1
 
-# Custom Function to get school website project
-function GET_SCHOOL() {
-    rm -rf school/
-    cd /tmp || exit 1
-    curl -sLo school.zip https://github.com/crazyuploader/school/archive/master.zip
-    unzip -d temp/ school.zip 1> /dev/null
-    rm -rf school.zip
-    cd temp || exit 1
-    mv school-master/ school/
-    mv school/ ~/public
-    cd ~/public || exit 1
-}
-
 # Pushing built to GitHub
 git config user.email "49350241+crazyuploader@users.noreply.github.com"
 git config user.name "crazyuploader"
-GET_SCHOOL
 echo ""
 git status
 git add .
